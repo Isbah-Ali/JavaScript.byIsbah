d3da1b14a7ea2deeda8951691b26961ab41aaf40
Isbah-Ali <isbahali17@gmail.com>
2025-03-24 09:25:09 +0000

    Data types in js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // it wont work // we are using node.js not browser
+
+// typeof      [returns the data type of variable]
+// instanceof 	Returns true if an object is an instance of an object type
+
+// number // 2 to the power of 53
+// bigint // not much used
+// string ""
+// boolean  // true/false
+// null // standalone value  //empty  // let state = null;
+// undefined // declared but not assigned any value // let state;
+// symbol  == unique
