87c0438d6c6c410a21f64684e4a9593b02b0cd10
Isbah-Ali <isbahali17@gmail.com>
2025-03-25 10:28:47 +0000

    Stack AND Heap Memory Management

---
+++ 01_basics/Stack&Heap.js
@@ -0,0 +1,14 @@
+// STACK  {Stores Primitive Datatypes}
+let myname = "Isbah Ali"
+
+let mysister = myname
+mysister = "Ayesha ALi"
+
+console.log(myname);
+console.log(mysister);
+
+
+
+
+
+// HEAP   {Stores Non-Primitve Datatypes}
