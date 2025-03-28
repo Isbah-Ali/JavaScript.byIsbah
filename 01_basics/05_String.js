3c351d77b24bea65174c4e8793333ce22d6c84c7
Isbah-Ali <isbahali17@gmail.com>
2025-03-28 17:02:47 +0000

    Strings in JS

---
+++ 01_basics/05_String.js
@@ -0,0 +1,9 @@
+// STRING INTERPOLATION
+let name = "Isbah Ali"
+let age = 21
+console.log(`Hello I am ${name} and my age is ${age}.`);
+
+// Creating String Object (using keyword new)
+const myname = new String ("Ayesha Ali")
+console.log(myname[3])
+console.log(myname.__proto__)  // shows that it is an object
