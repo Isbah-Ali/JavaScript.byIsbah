e5118f5656c2f9da92363aa9b5654ff48f1b1a0e
Isbah-Ali <isbahali17@gmail.com>
2025-04-07 19:01:41 +0000

    Functions in JS

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,14 @@
+function add_num(num1, num2){
+    return num1+num2
+}
+console.log(add_num(2,4))
+
+// OR:
+
+function multiply(num1, num2){
+    console.log(num1*num2)
+}
+multiply(3,6)
+
+// JavaScript donot checks datatypes we have to do  it manually
+//  with 3 + null only 3 will be printed
