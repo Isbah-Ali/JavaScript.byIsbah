71bbdad571632a351106274b284783e7f151136b
Isbah-Ali <isbahali17@gmail.com>
2025-04-14 14:18:42 +0000

    Functions Part 1 is completed

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -20,3 +20,19 @@ function logged_in_msg(username){
 }
 
 console.log(logged_in_msg("Isbah"))    // OUTPUT: Isbah jsut logged in
+
+console.log(logged_in_msg())    // OUTPUT: Undefined jsut logged in
+
+
+
+function loggedIn(username){
+    if (username === undefined){ // if (!username) THIS IS MORE GOOD CODE bec undefined is FALSE 
+       // so means if username is not TRUE, it is FALSE i.e undefined, hence then the block will run
+        return `Please enter a username`;
+    }
+
+    return `${username} just logged in`
+}
+
+console.log(loggedIn());
+console.log(loggedIn("Ayesha"));
