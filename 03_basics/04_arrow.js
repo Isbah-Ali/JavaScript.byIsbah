62298b4bc6569018b0c226826a60d4abba56f07d
Isbah-Ali <isbahali17@gmail.com>
2025-04-25 18:07:48 +0000

    Starting new topic: this keyword and arrows in JS

---
+++ 03_basics/04_arrow.js
@@ -0,0 +1,9 @@
+// THIS and arrow function in JavaScript
+// this is used to refer current context(values)
+const user = {
+  name: "Isbah Ali"
+
+  welcomeMessage: function(){
+    console.log(`${this.name} , Welcome to website!`)
+  }
+}
